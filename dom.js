f02caff85974a9a789dc90518e13645203161951
Frank Zhu <qf.zhu.frank@gmail.com>
2022-11-17 17:46:08 +0800

    v1.0.7

--- dom.js
+++ dom.js
@@ -10,9 +10,9 @@ function querySelect(cssId, component) {
   });
 }
 
-const getElementHeight = (cssId, component) => new Promise((resolve) => {
+const getElementHeight = (cssId, component) => new Promise((resolve, reject) => {
   if (!component) {
-    resolve('missing component');
+    reject(new Error('missing component'));
   } else {
     querySelect(cssId, component).then((res) => {
       if (Array.isArray(res[0])) {
@@ -20,7 +20,23 @@ const getElementHeight = (cssId, component) => new Promise((resolve) => {
       } else if (res[0] && res[0].height) {
         resolve(res[0].height);
       } else {
-        resolve('not found');
+        reject(new Error('element not found'));
+      }
+    });
+  }
+});
+
+const getElementWidth = (cssId, component) => new Promise((resolve, reject) => {
+  if (!component) {
+    reject(new Error('missing component'));
+  } else {
+    querySelect(cssId, component).then((res) => {
+      if (Array.isArray(res[0]) && res[0].length) {
+        resolve(Math.max(...res[0].map((x) => x.width)));
+      } else if (res[0] && res[0].width) {
+        resolve(res[0].width);
+      } else {
+        reject(new Error('element not found'));
       }
     });
   }
@@ -29,13 +45,13 @@ const getElementHeight = (cssId, component) => new Promise((resolve) => {
 const getNavHeight = () => {
   const navbar = getCurrentPages().pop().selectComponent('#navbar');
   return getElementHeight('#navbar', navbar);
-}
+};
 
 const getTabHeight = () => {
   const tabbar = getCurrentPages().pop().selectComponent('#tabbar');
   return getElementHeight('#tabbar', tabbar);
-}
+};
 
 module.exports = {
-  querySelect, getElementHeight, getNavHeight, getTabHeight,
+  querySelect, getElementHeight, getElementWidth, getNavHeight, getTabHeight,
 };
